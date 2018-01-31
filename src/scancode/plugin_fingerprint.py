4a4325b2778f7d54e7aa79c6abfc42f9e588dc7f
Jonathan "Jono" Yang <jyang@nexb.com>
2018-01-31 12:28:55 -0800

    Merge fingerprint and Merkle tree plugin into one file
    * Update test to reflect fingerprint field change (from `fingerprints` to `fingerprint`)
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- src/scancode/plugin_fingerprint.py
+++ src/scancode/plugin_fingerprint.py
@@ -30,6 +30,8 @@ from __future__ import unicode_literals
 from collections import OrderedDict
 
 from commoncode import filetype
+from plugincode.post_scan import PostScanPlugin
+from plugincode.post_scan import post_scan_impl
 from plugincode.scan import ScanPlugin
 from plugincode.scan import scan_impl
 from scancode import CommandLineOption
@@ -39,19 +41,19 @@ from scancode import SCAN_GROUP
 @scan_impl
 class FingerprintScanner(ScanPlugin):
     """
-    Calculate the Halo Hash and Hailstorm fingerprint value of a Resource.
+    Calculate the Halo Hash and Hailstorm fingerprint values of a Resource.
     """
     sort_order = 4
 
     options = [
-        CommandLineOption(('-g', '--fingerprints',),
+        CommandLineOption(('-g', '--fingerprint',),
             is_flag=True, default=False,
             help='Calculate the Halo Hash and Hailstorm fingerprint values for <input>.',
             help_group=SCAN_GROUP)
     ]
 
     def is_enabled(self):
-        return self.is_command_option_enabled('fingerprints')
+        return self.is_command_option_enabled('fingerprint')
 
     def get_scanner(self, **kwargs):
         return get_fingerprint
@@ -94,11 +96,12 @@ def get_fingerprint(location, **kwargs):
 
     selected_slices = list(select_ngrams(slices))
 
-    # Check to see if the first and last ngrams were selected
+    # Check to see if the first and last ngrams were selected,
+    # as stipulated in the Hailstorm algorithm
     first_ngram = list(ngrams(first_chunk, ngram_length))[0]
     last_ngram = list(ngrams(last_processed_chunk, ngram_length))[-1]
-    assert first_ngram in selected_slices
-    assert last_ngram in selected_slices
+    assert first_ngram == selected_slices[0]
+    assert last_ngram == selected_slices[-1]
 
     # Join slices together as a single bytestring
     hashable = b''.join(b''.join(slice) for slice in selected_slices)
@@ -110,3 +113,74 @@ def get_fingerprint(location, **kwargs):
     fingerprints['hailstorm'] = hailstorm.hexdigest()
 
     return [fingerprints]
+
+
+@post_scan_impl
+class MerkleTree(PostScanPlugin):
+    """
+    Compute a Merkle fingerprint for each directory using existing SHA1 and
+    Bit Average Halo hash values of the directories and files within the codebase
+    """
+
+    needs_info = True
+
+    def is_enabled(self):
+        return self.is_command_option_enabled('fingerprints')
+
+    def process_codebase(self, codebase, **kwargs):
+        """
+        Compute a Merkle fingerprint for each directory using existing SHA1 and
+        Bit Average Halo hash values of the directories and files within the codebase
+        """
+        from hashlib import sha1
+        from scancode.halohash import BitAverageHaloHash
+
+        # We walk bottom-up to ensure we process the children of directories
+        # before we calculate and assign the Merkle fingerprint for directories
+        for resource in codebase.walk(topdown=False):
+            if resource.children():
+                sha1s = []
+                bah128s = []
+                for child in resource.children():
+                    sha1 = child.sha1
+                    if sha1:
+                        sha1s.append(bytes(sha1))
+                    m_sha1 = get_fingerprint_field(child, 'merkle_sha1')
+                    if m_sha1:
+                        sha1s.append(bytes(m_sha1))
+
+                    bah128 = get_fingerprint_field(child, 'bah128')
+                    if bah128:
+                        bah128s.append(bytes(bah128))
+                    m_bah128 = get_fingerprint_field(child, 'merkle_bah128')
+                    if m_bah128:
+                        bah128s.append(bytes(m_bah128))
+
+                merkle_sha1 = sha1(b''.join(sorted(sha1s))).hexdigest()
+                set_fingerprint_field(resource, 'merkle_sha1', merkle_sha1)
+
+                merkle_bah128 = BitAverageHaloHash(b''.join(sorted(bah128s))).hexdigest()
+                set_fingerprint_field(resource, 'merkle_bah128', merkle_bah128)
+
+
+def get_fingerprint_field(resource, field):
+    scans = resource.get_scans()
+    if not scans:
+        return
+    fingerprints = scans.get('fingerprint', [])
+    if fingerprints:
+        fingerprint = fingerprints[0]
+        return fingerprint.get(field) or None
+
+
+def set_fingerprint_field(resource, field, field_value):
+    scans = resource.get_scans()
+    fingerprints = scans.get('fingerprint', [])
+    if fingerprints:
+        fingerprint = fingerprints[0]
+    else:
+        fingerprint = OrderedDict()
+        fingerprints.append(fingerprint)
+    fingerprint[field] = field_value
+    scans['fingerprint'] = fingerprints
+    resource.put_scans(scans)
